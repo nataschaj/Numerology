b6baf6815e1d54b19f781c21cd4cf3d9636c7b55
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-10-26 18:58:16 +0200

    Two numbers added in InsertCodeHere

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,8 +8,11 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
             MysticNumbers newNumber = new MysticNumbers();
+            Console.WriteLine($"{newNumber.ThreeNumbers(23, 20, 19)}");
 
-            
+            Console.WriteLine($"{newNumber.TwoNumbers(23, 20)}");
+
+
             // The LAST line of code should be ABOVE this line
         }
     }
